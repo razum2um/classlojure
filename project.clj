52a1b50a35c2039c800c2a151b651bac135c5970
Vlad Bokov <razum2um@mail.ru>
2016-03-25 15:56:38 +0600

    own project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.flatland/classlojure "0.7.1"
-  :url "https://github.com/flatland/classlojure"
+(defproject razum2um/classlojure "0.7.2"
+  :url "https://github.com/razum2um/classlojure"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Advanced classloading for clojure."
-  :dependencies [[org.clojure/clojure "1.5.0"]]
-  :aliases {"testall" ["with-profile" "dev,default:dev,1.4,default:dev,1.6,default" "test"]}
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.6,default:dev,1.7,default" "test"]}
+  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}})
